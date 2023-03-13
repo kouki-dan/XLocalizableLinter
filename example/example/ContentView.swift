1bd2def26de117cb244c02d0ffaccade16d4dd6c
Kouki Saito <dan.addr.skd@gmail.com>
2023-03-14 00:24:20 +0900

    Let's fail

--- example/example/ContentView.swift
+++ example/example/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, world?")
             Text(verbatim: NSLocalizedString("NSLocalizedString", comment: "This is from NSLocalizedString"))
             Text(
                 "\"Keys may include double quote and equals(==) and backslash(\\)\"\"",
